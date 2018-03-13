93f9d2fb3202b64559e24da5911c2cc8bbd26a27
Harper Jiang <harperjiang@msn.com>
2018-03-13 16:47:50 -0500

    ready for all performance

--- test/scan/bitpack/HaoScanner128_test.cpp
+++ test/scan/bitpack/HaoScanner128_test.cpp
@@ -110,48 +110,50 @@ TEST(HaoScanner128, TestAlignedEq) {
     free(output);
 }
 
-TEST(HaoScanner128, TestUnalignedRange) {
+TEST(HaoScanner128, TestUnalignedLess) {
     int entrySize = 9;
-    int data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
+    int data[] = {3, 4, 9, 10, 22, 1, 7, 6, 8, 5, 6, 11, 12, 13, 14, 7, 8, 9, 10, 15, 16, 17,
                   18, 19, 20};
-    int result[3];
-    int output[30];
-    encode(data, result, 20, entrySize);
+    int *encoded = (int *) aligned_alloc(16, 8 * sizeof(int));
+    int *output = (int *) aligned_alloc(16, 8 * sizeof(int));
+    encode(data, encoded, 25, entrySize);
 
     HaoScanner128 *scanner = new HaoScanner128(entrySize, false);
 
-    Predicate p(opr_in, 3, 8);
+    Predicate p(opr_in, 8, 0);
 
-    scanner->scan(result, 20, output, &p);
-    for (int i = 0; i < 20; i++) {
+    scanner->scan(encoded, 25, output, &p);
+    for (int i = 0; i < 25; i++) {
         int bitOffset = (i + 1) * entrySize - 1;
         int intIdx = bitOffset / 32;
         int bitIdx = bitOffset % 32;
-        if (i >= 2 && i <= 6)
+        if (data[i] >= 8)
             EXPECT_EQ(1 << bitIdx, output[intIdx] & (1 << bitIdx)) << i;
         else
             EXPECT_EQ(0, output[intIdx] & (1 << bitIdx)) << i;
     }
+    free(encoded);
+    free(output);
 }
 
-TEST(HaoScanner128, TestAlignedRange) {
+TEST(HaoScanner128, TestAlignedLess) {
     int entrySize = 9;
-    int data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
+    int data[] = {3, 4, 9, 10, 22, 1, 7, 6, 8, 5, 6, 11, 12, 13, 14, 7, 8, 9, 10, 15, 16, 17,
                   18, 19, 20};
     int *encoded = (int *) aligned_alloc(16, 8 * sizeof(int));
     int *output = (int *) aligned_alloc(16, 8 * sizeof(int));
-    encode(data, encoded, 20, entrySize);
+    encode(data, encoded, 25, entrySize);
 
     HaoScanner128 *scanner = new HaoScanner128(entrySize, true);
 
-    Predicate p(opr_in, 3, 8);
+    Predicate p(opr_in, 8, 0);
 
-    scanner->scan(encoded, 20, output, &p);
-    for (int i = 0; i < 20; i++) {
+    scanner->scan(encoded, 25, output, &p);
+    for (int i = 0; i < 25; i++) {
         int bitOffset = (i + 1) * entrySize - 1;
         int intIdx = bitOffset / 32;
         int bitIdx = bitOffset % 32;
-        if (i >= 2 && i <= 6)
+        if (data[i] >= 8)
             EXPECT_EQ(1 << bitIdx, output[intIdx] & (1 << bitIdx)) << i;
         else
             EXPECT_EQ(0, output[intIdx] & (1 << bitIdx)) << i;
