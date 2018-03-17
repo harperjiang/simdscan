99dd6397d60942ceada62c1f85803b24ee5f7733
Hao Jiang <harperjiang@msn.com>
2018-03-17 10:10:23 -0500

    Update delta scan

--- test/scan/delta/SimdDeltaScanner16_test.cpp
+++ test/scan/delta/SimdDeltaScanner16_test.cpp
@@ -54,9 +54,9 @@ TEST(SimdDeltaScanner16, less) {
 
     for (int i = 0; i < 24; i++) {
         if (cumsum[i] < 30)
-            EXPECT_EQ(1 << (7 - i % 8), result[i / 8] & (1 << (7 - i % 8)));
+            EXPECT_EQ(1 << (7 - i % 8), result[i / 8] & (1 << (7 - i % 8))) << i;
         else
-            EXPECT_EQ(0, (result[i / 8] & (1 << (7 - i % 8))));
+            EXPECT_EQ(0, (result[i / 8] & (1 << (7 - i % 8)))) << i;
     }
 
     delete[] cumsum;
