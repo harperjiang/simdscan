67394ced9ee1f5109147bad7fd345aca1ee6f65b
Hao Jiang <harperjiang@msn.com>
2018-03-19 09:00:47 -0500

    RLE data collection

--- src/perf_rle.cpp
+++ src/perf_rle.cpp
@@ -10,9 +10,7 @@
 #include "scan/rle/SimdRLEScanner.h"
 
 
-uint64_t rle_throughput(Scanner *scanner, uint64_t num, int es, int rls) {
-    int *input = (int *) aligned_alloc(64, sizeof(int) * num);
-
+uint64_t rle_throughput(Scanner *scanner, uint64_t num, int es, int rls, int *input, int *output, int *encoded) {
     std::mt19937 rng;
     rng.seed(std::random_device()());
     std::uniform_int_distribution<std::mt19937::result_type> dist; // distribution in range [1, 6]
@@ -21,10 +19,8 @@ uint64_t rle_throughput(Scanner *scanner, uint64_t num, int es, int rls) {
         input[i] = dist(rng);
     }
     // Large enough
-    int *encoded = (int *) aligned_alloc(64, sizeof(int) * 2 * num);
     int numRle = encode_rle(input, encoded, num, es, rls);
 
-    int *output = (int *) aligned_alloc(64, sizeof(int) * 2 * num);
 
     auto x = dist(rng);
 //    Predicate p(opr_in, x / 2, x);
@@ -43,15 +39,16 @@ uint64_t rle_throughput(Scanner *scanner, uint64_t num, int es, int rls) {
 
     // printf("%d\n", output[232]);
 
-    free(input);
-    free(output);
-    free(encoded);
 
     return num / elapse;
 }
 
 int main(int argc, char **argv) {
     uint64_t num = 100000000;
+    int *input = (int *) aligned_alloc(64, sizeof(int) * num);
+    int *encoded = (int *) aligned_alloc(64, sizeof(int) * 2 * num);
+    int *output = (int *) aligned_alloc(64, sizeof(int) * 2 * num);
+
     int MAX_REPEAT = 5;
     for (int rls = 7; rls < 32; rls++) {
         for (int es = 5; es < 32; es++) {
@@ -59,13 +56,17 @@ int main(int argc, char **argv) {
             uint64_t aligned = 0;
             uint64_t unaligned = 0;
             for (int repeat = 0; repeat < MAX_REPEAT; repeat++) {
-                trivial += rle_throughput(new TrivialRLEScanner(es, rls), num, es, rls);
-                aligned += rle_throughput(new SimdRLEScanner(es, rls, true), num, es, rls);
-                unaligned += rle_throughput(new SimdRLEScanner(es, rls, false), num, es, rls);
+                trivial += rle_throughput(new TrivialRLEScanner(es, rls), num, es, rls, input, output, encoded);
+                aligned += rle_throughput(new SimdRLEScanner(es, rls, true), num, es, rls, input, output, encoded);
+                unaligned += rle_throughput(new SimdRLEScanner(es, rls, false), num, es, rls, input, output, encoded);
             }
             std::cout << es << "," << rls << "," << (double) aligned / trivial << ","
                       << (double) unaligned / trivial << "," << trivial / MAX_REPEAT << ","
                       << aligned / MAX_REPEAT << "," << unaligned / MAX_REPEAT << std::endl;
         }
     }
+
+    free(input);
+    free(output);
+    free(encoded);
 }
