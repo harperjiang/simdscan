083563446ed34cc4b7cdd1286a92f3547f51929d
Harper Jiang <harperjiang@msn.com>
2018-03-21 12:29:09 -0500

    JNI call

--- src/jni/SimdScanner.cpp
+++ src/jni/SimdScanner.cpp
@@ -1,49 +1,64 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include "edu_uchicago_cs_encsel_query_simdscan_SimdScanner.h"
+#include "../scan/bitpack/HaoLaneLoaderScanner.h"
 
-void scanByByte(jbyte* input, jint offset, jint idx, jint target, jint entryWidth, jbyte* result, jlong& buffer);
-void scanInt(jbyte* input, jlong len, jint offset, jint size, jint target, jint entryWidth, jbyte* result);
-void scanLong(jbyte* input, jlong len, jint offset, jint size, jint target, jint entryWidth, jbyte* result);
+void scanByByte(jbyte *input, jint offset, jint idx, jint target, jint entryWidth, jbyte *result, jlong &buffer);
 
-jint getInt(jbyte* buffer, jint index) ;
-jlong getLong(jbyte* buffer, jint index) ;
-void putInt(jbyte* buffer, jint index, jint value);
-void putLong(jbyte* buffer, jint index, jlong value) ;
+void scanInt(jbyte *input, jlong len, jint offset, jint size, jint target, jint entryWidth, jbyte *result);
+
+void scanLong(jbyte *input, jlong len, jint offset, jint size, jint target, jint entryWidth, jbyte *result);
+
+jint getInt(jbyte *buffer, jint index);
+
+jlong getLong(jbyte *buffer, jint index);
+
+void putInt(jbyte *buffer, jint index, jint value);
+
+void putLong(jbyte *buffer, jint index, jlong value);
 
 
 static void init() __attribute__((constructor));
+
 static void release() __attribute__((destructor));
 
-static jbyte* memblock;
+static jbyte *memblock;
 static uint64_t memsize;
 
 void init() {
-    memsize = 100*1024*1024;
-    memblock = (jbyte*)malloc(memsize*sizeof(jbyte));
+    memsize = 100 * 1024 * 1024;
+    memblock = (jbyte *) aligned_alloc(64, memsize * sizeof(jbyte));
 }
 
 void release() {
     free(memblock);
 }
 
-JNIEXPORT jobject JNICALL Java_edu_uchicago_cs_encsel_query_simdscan_SimdScanner_scanBitpacked
-  (JNIEnv *env, jobject self, jobject input, jint offset, jint size, jint target, jint entryWidth) {
-   jlong resSize = (size * entryWidth / 64 + (((size * entryWidth) % 64)?1:0)) * 8;
-
-   if(resSize > memsize) {
-        memsize = resSize;
+void allocOutput(jlong outputSize) {
+    if (outputSize > memsize) {
+        memsize = outputSize;
         free(memblock);
-        memblock = (jbyte*) malloc(sizeof(jbyte)*memsize);
-   }
+        memblock = (jbyte *) aligned_alloc(64, sizeof(jbyte) * memsize);
+    }
+}
 
-   jbyte* array = (jbyte*)env->GetDirectBufferAddress(input);
-   jlong arrayLen = env->GetDirectBufferCapacity(input);
+JNIEXPORT jobject JNICALL Java_edu_uchicago_cs_encsel_query_simdscan_SimdScanner_scanBitpacked
+        (JNIEnv *env, jobject self, jobject input, jint offset, jint size, jint target, jint entryWidth) {
+    jbyte *array = (jbyte *) env->GetDirectBufferAddress(input);
+    jlong arrayLen = env->GetDirectBufferCapacity(input);
 
-   /**
-    * Determine a safe number
-    */
+    jint safeSize = (arrayLen - 1024) / entryWidth;
 
-   return env->NewDirectByteBuffer(memblock,resSize);
+    // Allocate a 64-bit for each entry, should be enough
+    allocOutput(safeSize * 8);
+
+    HaoLaneLoaderScanner *scanner = new HaoLaneLoaderScanner(entryWidth);
+    scanner->scan((int *) array, safeSize, (int *) memblock, new Predicate(opr_eq, target, 0));
+    /**
+     * Determine a safe number to leave enough space for Scanner to read, e.g. 2 SIMD words
+     */
+
+
+    return env->NewDirectByteBuffer(memblock, safeSize * 8);
 
 }
