0c0071581070a8de253d3386840a6412bda337f2
Hao Jiang <harperjiang@msn.com>
2018-05-15 01:37:55 -0500

    delta experiments

--- test/scan/delta/SimdDeltaScanner16_test.cpp
+++ test/scan/delta/SimdDeltaScanner16_test.cpp
@@ -67,7 +67,7 @@ TEST(SimdDeltaScanner16, cumsum) {
 
     __m256i input = _mm256_setr_epi16(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16);
 
-    SimdDeltaScanner16 *scanner = new SimdDeltaScanner16(29);
+    SimdDeltaScanner16 *scanner = new SimdDeltaScanner16(10);
 
     __m256i cumsum = scanner->cumsum(input);
 
